4f762515cfd8d611ba5a5ebda2a92eaa4da4dab3
GavrilStefan-Dorian <stefan.dorian.gavril@gmail.com>
2024-02-27 09:03:27 +0200

    Lab01

--- Lab01/src/Main.java
+++ Lab01/src/Main.java
@@ -1,3 +1,7 @@
+package Lab01.src;
+
+import static java.lang.Integer.parseInt;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,26 +9,28 @@ public class Main {
         String[] languages = {"C", "C++",
                 "C#", "Python", "Go", "Rust",
                 "JavaScript", "PHP", "Swift", "Java"};
-        int n = (int)(Math.random() * 1_000_000);
+        int n = (int) (Math.random() * 1_000_000);
 
         int result = n * 3;
-        n += 0b10101;
-        n += 0xFF;
-        n *= 6;
+        result += parseInt(String.valueOf(0b10101));
+        result += parseInt(String.valueOf(0xFF));
+        result *= 6;
 
-        int  i = 0;
+        int i = 0;
 
         do {
-            while (n > 0) {
-                i += n % 10;
-                n /= 10;
+            while (result > 0) {
+                i += result % 10;
+                result /= 10;
             }
+            result = i;
+            i = 0;
         }
-        while(i > 9);
+        while (result > 9);
 
+        System.out.print("Hello world!\n");
         System.out.printf("Willy-nilly, this semester I will learn " + languages[result]);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello world!");
     }
 }
